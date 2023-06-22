b8fc49f6b8017de073ca4f2bd9381886a6a5d7dc
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-06-22 16:02:08 +0400

    Tentative de correction de bug

--- frontend/src/services/TestApi.js
+++ frontend/src/services/TestApi.js
@@ -1,64 +1,66 @@
-import axios from "axios";
+import axios from 'axios'
 
 // Création d'une instance d'axios avec l'URL de base de votre API
 const baseURL = 'http://127.0.0.1:8000/api'
 
-  const  GameService = {
-    // Méthode pour récupérer tous les jeux
-    async getGames() {
-      return axios.get(baseURL + '/jeux/')
-        .then(response => {
-          console.log("ALL", response.data);
-          return response.data;
-        })
-        .catch(error => {
-          console.error(error);
-          throw error;
-        });
-    },
-  
-    // Méthode pour récupérer un jeu spécifique par son ID
-    async getGame(id) {
-      return axios.get( baseURL + '/jeux/' + id)
-      .then(response => {
-        console.log("ONE", response.data);
-        return response.data;
+const GameService = {
+  // Méthode pour récupérer tous les jeux
+  async getGames() {
+    return axios
+      .get(baseURL + '/jeux/')
+      .then((response) => {
+        console.log('ALL', response.data)
+        return response.data
       })
-      .catch(error => {
-        console.error(error);
-        throw error;
-      });
-    },
-    // Méthode pour créer un nouveau jeu
-    async createGame(game) {
-      try {
-        const response = await axios.post(baseURL + '/jeux', game);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    },
-    
-    async updateGame(id, game) {
-      try {
-        const response = await axios.put(baseURL + '/jeux/' + id, game);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    },
-    
-    async deleteGame(id) {
-      try {
-        const response = await axios.delete(baseURL + '/jeux/' + id);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    }    
+      .catch((error) => {
+        console.error(error)
+        throw error
+      })
+  },
+
+  // Méthode pour récupérer un jeu spécifique par son ID
+  async getGame(id) {
+    return axios
+      .get(baseURL + '/jeux/' + id)
+      .then((response) => {
+        console.log('ONE', response.data)
+        return response.data
+      })
+      .catch((error) => {
+        console.error(error)
+        throw error
+      })
+  },
+  // Méthode pour créer un nouveau jeu
+  async createGame(game) {
+    try {
+      const response = await axios.post(baseURL + '/jeux', game)
+      return response.data
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  },
+
+  async updateGame(id, game) {
+    try {
+      const response = await axios.put(baseURL + '/jeux/' + id, game)
+      return response.data
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  },
+
+  async deleteGame(id) {
+    try {
+      const response = await axios.delete(baseURL + '/jeux/' + id)
+      return response.data
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  }
 }
-  
-export default GameService;
+
+export default GameService
--- frontend/src/views/__tests__/TableView.spec.js
+++ frontend/src/views/__tests__/TableView.spec.js
@@ -10,8 +10,15 @@ const mockGet = {
   ]
 }
 
+const mockGetOne = {
+    data: [
+      { id: 1, nombre_joueur: 10, nom_jeu: 'Monopoly', age: 20 },
+    ]
+  }
+
 // Spy sur la méthode 'get' de l'objet axios et renvoie une valeur résolue (mockGet) pour simuler une requête réussie
 vi.spyOn(axios, 'get').mockResolvedValue(mockGet)
+// vi.spyOn(axios, 'get').mockResolvedValue(mockGetOne)
 // fn va capturer l'appel à l'api et to permet de faire des callbakc dessus, à utiliser avec le serveur lancé
 // vi.fn(axios, 'get').mockResolvedValue(mockGet)
 // Définition d'un bloc de tests avec la description "Get all games"
@@ -23,8 +30,8 @@ describe('Get games', () => {
     expect(data).toHaveLength(2)
   })
   it('GET ONE', async () => {
-    const result = await GameService.getGame(12)
-    expect(axios.get).toHaveBeenCalledOnce()
+    const result = await GameService.getGame(1)// Appel à la méthode getGame() du service GameService avec l'ID 12
+    expect(axios.get).toHaveBeenCalled()
     expect(result).toHaveLength(1)
   })
 })
