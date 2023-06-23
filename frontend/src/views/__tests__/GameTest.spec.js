8b5a5ea4a5e15947c90282d4c474a53bfddaeeca
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-06-23 15:38:39 +0400

    Update GameTest.spec.js

--- frontend/src/views/__tests__/GameTest.spec.js
+++ frontend/src/views/__tests__/GameTest.spec.js
@@ -1,16 +1,16 @@
 import GameService from '../../services/GamesService'
-import Mocks from '../../__mocks__/GameMock';
+import Mocks from '../../__mocks__/GameMock'
 import axios from 'axios'
 import { describe, it, expect, vi } from 'vitest'
 
 // Spy sur la méthode 'get' de l'objet axios et renvoie une valeur résolue (mockGet) pour simuler une requête réussie
 vi.spyOn(axios, 'get').mockImplementation((url) => {
-    if (url === 'http://127.0.0.1:8000/api/jeux/1') {
-      return Promise.resolve(Mocks.getOne)
-    } else {
-        return Promise.resolve(Mocks.get)
-    }
-  })
+  if (url === GameService.baseURL + '1') {
+    return Promise.resolve(Mocks.getOne)
+  } else {
+    return Promise.resolve(Mocks.get)
+  }
+})
 vi.spyOn(axios, 'delete').mockResolvedValue(Mocks.delete_)
 vi.spyOn(axios, 'patch').mockResolvedValue(Mocks.update)
 vi.spyOn(axios, 'post').mockResolvedValue(Mocks.create)
@@ -19,33 +19,37 @@ vi.spyOn(axios, 'post').mockResolvedValue(Mocks.create)
 // vi.fn(axios, 'get').mockResolvedValue(mockGet)
 // Définition d'un bloc de tests avec la description "Get all games"
 describe('Get games', () => {
-    // Définition d'un test avec la description "should return"
-    it('GET ALL', async () => {
-        const data = await GameService.getGames()
-        expect(axios.get).toHaveBeenCalledOnce()
-        expect(data).toMatchObject(Mocks.get.data)
+  // Définition d'un test avec la description "should return"
+  it('GET ALL', async () => {
+    const data = await GameService.getGames()
+    expect(axios.get).toHaveBeenCalledOnce()
+    expect(data).toMatchObject(Mocks.get.data)
+  })
+  it('GET ONE', async () => {
+    const result = await GameService.getGame(1)
+    expect(axios.get).toHaveBeenCalledWith(GameService.baseURL + '1')
+    expect(result).toMatchObject(Mocks.getOne.data)
+  })
+  it('DELETE', async () => {
+    const result = await GameService.deleteGame(12)
+    expect(axios.delete).toHaveBeenCalledOnce()
+    expect(result).not.toContain(12)
+  })
+  it('PATCH', async () => {
+    // Tester ce qui a été passé à Axios
+    const result = await GameService.updateGame(12, { age: '+99' })
+    expect(axios.patch).toHaveBeenCalledOnce()
+    expect(result[0].age).toEqual('+99')
+  })
+  it('POST', async () => {
+    const result = await GameService.createGame(12, {
+      id: 1,
+      nombre_joueur: 10,
+      nom_jeu: 'Monopoly',
+      age: 20
     })
-    it('GET ONE', async () => {
-        const result = await GameService.getGame(1)
-        expect(axios.get).toHaveBeenCalledWith(GameService.baseURL + '1')
-        expect(result).toMatchObject(Mocks.getOne.data)
-    })
-    it('DELETE', async () => {
-        const result = await GameService.deleteGame(12)
-        expect(axios.delete).toHaveBeenCalledOnce()
-        expect(result).not.toContain(12)
-    })
-    it('PATCH', async () => {
-        // Tester ce qui a été passé à Axios
-        const result = await GameService.updateGame(12, {age:"+99"})
-        expect(axios.patch).toHaveBeenCalledOnce()
-        expect(result[0].age).toEqual("+99")
-    })
-    it('POST', async () => {
-        const result = await GameService.createGame(12,
-            { id: 1, nombre_joueur: 10, nom_jeu: 'Monopoly', age: 20 })
-        expect(axios.post).toHaveBeenCalledOnce()
-        expect(result[0].id).toEqual(1)
-        expect(result[0].nombre_joueur).toEqual(10)
-    })
+    expect(axios.post).toHaveBeenCalledOnce()
+    expect(result[0].id).toEqual(1)
+    expect(result[0].nombre_joueur).toEqual(10)
+  })
 })
